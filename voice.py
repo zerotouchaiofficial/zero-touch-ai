df8bbe5e1d67bed1ce376a1a9b2ee558bc0b88f4
zerotouchaiofficial <zerotouchai.official@gmail.com>
2026-01-02 04:05:32 -0800

    Update voice.py

--- voice.py
+++ voice.py
@@ -3,7 +3,7 @@ import os
 
 tts = TTS(model_name="tts_models/en/vctk/vits")
 
-def make_voice(text):
+def generate_voice(text):
     os.makedirs("output", exist_ok=True)
     out = "output/audio.wav"
     tts.tts_to_file(text=text, file_path=out)
